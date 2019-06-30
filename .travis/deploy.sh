16c5f81f81eea3d317d6e859f31f870d88c3fdf7
Benjamin Benoist <benjamin.benoist@snowplowanalytics.com>
2019-07-01 00:23:08 +0200

    use beforedepoy

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -6,9 +6,6 @@ cd $TRAVIS_BUILD_DIR
 
 if [[ $TRAVIS_TAG =~ ^micro.* ]]
 then
-  echo "Deploying SSC locally"
-  git clone git@github.com:snowplow/snowplow.git && cd snowplow/2-collectors/scala-stream-collector/ && sbt publishLocal && cd ../../..
-  echo "Compiling and publishing Micro to Bintray"
   sbt docker:publish
 else
   echo "Tag doesn't start with micro and isn't a release tag."
