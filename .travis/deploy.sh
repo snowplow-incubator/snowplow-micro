6d6607ee41888c00cbd05b4a06de7ab8cf24c77f
Benjamin Benoist <benjamin.benoist@snowplowanalytics.com>
2019-07-01 00:08:04 +0200

    compile ssc first

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -6,6 +6,7 @@ cd $TRAVIS_BUILD_DIR
 
 if [[ $TRAVIS_TAG =~ ^micro.* ]]
 then
+  git clone git@github.com:snowplow/snowplow.git && cd snowplow/2-collectors/scala-stream-collector/ && sbt publishLocal && cd ../../..
   sbt docker:publish
 else
   echo "Tag doesn't start with micro and isn't a release tag."
