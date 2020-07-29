082c4ece413255c963adff62d4d2410b0d34b2e2
Ian Streeter <ian@edelweissconnect.com>
2020-07-29 06:45:44 +0100

    Fix filtering on event_type and schema (closes #24)

--- src/test/scala/com.snowplowanalytics.snowplow.micro/ValidationCacheSpec.scala
+++ src/test/scala/com.snowplowanalytics.snowplow.micro/ValidationCacheSpec.scala
@@ -14,169 +14,342 @@ package com.snowplowanalytics.snowplow.micro
 
 import org.specs2.mutable.Specification
 
+import com.snowplowanalytics.snowplow.enrich.common.loaders.CollectorPayload
+import com.snowplowanalytics.snowplow.enrich.common.outputs.EnrichedEvent
+
 class ValidationCacheSpec extends Specification {
+  import ValidationCacheSpec._
 
-  val emptyFilterGood = FiltersGood(None, None, None, None)
-  val emptyFilterBad = FiltersBad(None, None, None)
-
-  // create good event 1
-  // create good event 2
-  // create good event 3
-
-  // create bad event 1
-  // create bad event 2
 
 
   "getSummary" >> {
     "should return the correct number of bad and good events" >> {
-      ValidationCache.getSummary().good must_== ValidationCache.filterGood(emptyFilterGood).size
-      ValidationCache.getSummary().bad must_== ValidationCache.filterBad(emptyFilterBad).size
-      ValidationCache.getSummary().total must_== ValidationCache.filterGood(emptyFilterGood).size +
-        ValidationCache.filterBad(emptyFilterBad).size
+      "for an empty cache" >> {
+        val cache = emptyCache()
 
-      // add 2 good and 1 bad
+        cache.getSummary().good must_== 0
+        cache.getSummary().bad must_== 0
+        cache.getSummary().total must_== 0
+      }
 
-      ValidationCache.getSummary().good must_== ValidationCache.filterGood(emptyFilterGood).size
-      ValidationCache.getSummary().bad must_== ValidationCache.filterBad(emptyFilterBad).size
-      ValidationCache.getSummary().total must_== ValidationCache.filterGood(emptyFilterGood).size +
-        ValidationCache.filterBad(emptyFilterBad).size
+      "for a non-empty cache" >> {
+        val cache = cacheOf(
+          List(GoodEvent1, GoodEvent2),
+          List(BadEvent1)
+          )
+
+        cache.getSummary().good must_== 2
+        cache.getSummary().bad must_== 1
+        cache.getSummary().total must_== 3
+
+        cache.filterGood() must contain(exactly(GoodEvent1, GoodEvent2))
+        cache.filterBad() must contain(exactly(BadEvent1))
+      }
     }
   }
 
   "addToGood" >> {
     "should succesfully add one good event to an empty cache" >> {
-      ValidationCache.reset()
-      // add event 1
-      // check that cache contains only it
-      1 must_== 1
+      val cache = emptyCache()
+      cache.addToGood(List(GoodEvent1))
+
+      cache.getSummary().good must_== 1
+      cache.getSummary().bad must_== 0
+      cache.filterGood() must contain(exactly(GoodEvent1))
     }
 
-    // might have to deactivate parallelism
     "should succesfully add several good events to an empty cache" >> {
-      ValidationCache.reset()
-      // add event 1 and 2
-      // check that cache contains only these 2
-      1 must_== 1
+      val cache = emptyCache()
+      cache.addToGood(List(GoodEvent1, GoodEvent2))
+
+      cache.getSummary().good must_== 2
+      cache.getSummary().bad must_== 0
+      cache.filterGood() must contain(exactly(GoodEvent1, GoodEvent2))
     }
     
     "should succesfully add good events to a non empty cache" >> {
-      ValidationCache.reset()
-      // add event 1
-      // add event 2 and 3
-      // check that cache contains them 
-      1 must_== 1
+      val cache = cacheOf(List(GoodEvent1), Nil)
+      cache.addToGood(List(GoodEvent2))
+
+      cache.getSummary().good must_== 2
+      cache.getSummary().bad must_== 0
+      cache.filterGood() must contain(exactly(GoodEvent1, GoodEvent2))
     }
   }
   
   "addToBad" >> {
     "should succesfully add one bad event to an empty cache" >> {
-      ValidationCache.reset()
-      // add event 1
-      // check that cache contains only it
-      1 must_== 1
+      val cache = emptyCache()
+      cache.addToBad(List(BadEvent1))
+
+      cache.getSummary().good must_== 0
+      cache.getSummary().bad must_== 1
+      cache.filterBad() must contain(exactly(BadEvent1))
     }
 
-    // might have to deactivate parallelism
     "should succesfully add several bad events to an empty cache" >> {
-      ValidationCache.reset()
-      // add event 1 and 2
-      // check that cache contains only these 2
-      1 must_== 1
+      val cache = emptyCache()
+      cache.addToBad(List(BadEvent1, BadEvent2))
+
+      cache.getSummary().good must_== 0
+      cache.getSummary().bad must_== 2
+      cache.filterBad() must contain(exactly(BadEvent1, BadEvent2))
     }
     
     "should succesfully add bad events to a non empty cache" >> {
-      ValidationCache.reset()
-      // add event 1
-      // add event 2 and 3
-      // check that cache contains them 
-      1 must_== 1
+      val cache = cacheOf(Nil, List(BadEvent1))
+      cache.addToBad(List(BadEvent2))
+
+      cache.getSummary().good must_== 0
+      cache.getSummary().bad must_== 2
+      cache.filterBad() must contain(exactly(BadEvent1, BadEvent2))
     }
   }
 
   "reset" >> {
     "should remove all the good and bad events from the cache" >> {
-      // add a good event
-      // add a bad event
-      // check not empty
-      ValidationCache.reset()
-      ValidationCache.getSummary().good must_== 0
-      ValidationCache.getSummary().bad must_== 0
+      val cache = cacheOf(List(GoodEvent1), List(BadEvent1))
+      cache.reset()
+      cache.getSummary().good must_== 0
+      cache.getSummary().bad must_== 0
     }
   }
 
   "filterGood" >> {
-    "should return only the good events for which the filter matches" >> {
-      // put 3 events in cache
-      // filter that returns all of them
-      // filter that returns 2 of them
-      // filter that returns 1 of them
-      // filter that returns none of them
-      1 must_== 1
+    "should return only the good events that match the filtered event type" >> {
+      val cache = cacheOf(List(GoodEvent1, GoodEvent2), Nil)
+
+      val filter1 = EmptyFilterGood.copy(event_type = GoodEvent1.eventType)
+      val filter2 = EmptyFilterGood.copy(event_type = GoodEvent2.eventType)
+      val filterNonsense = EmptyFilterGood.copy(event_type = Some("nonsense"))
+
+      cache.filterGood(filter1) must contain(exactly(GoodEvent1))
+      cache.filterGood(filter2) must contain(exactly(GoodEvent2))
+      cache.filterGood(filterNonsense) must be empty
+    }
+
+    "should return only the good events that match the filtered schema" >> {
+      val cache = cacheOf(List(GoodEvent1, GoodEvent2), Nil)
+
+      val filter1 = EmptyFilterGood.copy(schema = GoodEvent1.schema)
+      val filter2 = EmptyFilterGood.copy(schema = GoodEvent2.schema)
+      val filterNonsense = EmptyFilterGood.copy(schema = Some("nonsense"))
+
+      cache.filterGood(filter1) must contain(exactly(GoodEvent1))
+      cache.filterGood(filter2) must contain(exactly(GoodEvent2))
+      cache.filterGood(filterNonsense) must be empty
+    }
+
+    "should return only the good events that match the filtered context" >> {
+      val cache = cacheOf(List(GoodEvent1, GoodEvent2), Nil)
+
+      val filter1a = EmptyFilterGood.copy(contexts = Some(GoodEvent1.contexts.headOption.toList))
+      val filter1b = EmptyFilterGood.copy(contexts = Some(GoodEvent1.contexts.tail))
+      val filter1c = EmptyFilterGood.copy(contexts = Some(GoodEvent1.contexts))
+
+      val filter2a = EmptyFilterGood.copy(contexts = Some(GoodEvent2.contexts.headOption.toList))
+      val filter2b = EmptyFilterGood.copy(contexts = Some(GoodEvent2.contexts.tail))
+      val filter2c = EmptyFilterGood.copy(contexts = Some(GoodEvent2.contexts))
+
+      
+      val filterAllContexts = EmptyFilterGood.copy(contexts = Some(GoodEvent1.contexts ::: GoodEvent2.contexts))
+      val filterNonsense = EmptyFilterGood.copy(contexts = Some(List("nonsense")))
+
+
+      cache.filterGood(filter1a) must contain(exactly(GoodEvent1))
+      cache.filterGood(filter1b) must contain(exactly(GoodEvent1))
+      cache.filterGood(filter1c) must contain(exactly(GoodEvent1))
+
+      cache.filterGood(filter2a) must contain(exactly(GoodEvent2))
+      cache.filterGood(filter2b) must contain(exactly(GoodEvent2))
+      cache.filterGood(filter2c) must contain(exactly(GoodEvent2))
+
+      cache.filterGood(filterAllContexts) must have size(0)
+      cache.filterGood(filterNonsense) must have size(0)
     }
 
     "should return the most recent events if limit is set" >> {
-      1 must_== 1
+      val cache = emptyCache()
+      cache.addToGood(List(GoodEvent1))
+      cache.addToGood(List(GoodEvent2)) // most recent
+
+      val filter = EmptyFilterGood.copy(limit = Some(1))
+      cache.filterGood(filter) must contain(exactly(GoodEvent2))
     }
   }
 
   "keepGoodEvent" >> {
     "should correctly return true or false if the filter contains only an event_type and it matches" >> {
-      // true if same event type
-      // false if not
-      1 must_== 1
+      val shouldKeep = EmptyFilterGood.copy(event_type = GoodEvent1.eventType)
+      val shouldNotKeep = EmptyFilterGood.copy(event_type = GoodEvent2.eventType)
+
+      ValidationCache.keepGoodEvent(GoodEvent1, shouldKeep) should beTrue
+      ValidationCache.keepGoodEvent(GoodEvent1, shouldNotKeep) should beFalse
     }
     
     "should correctly return true or false if the filter contains only a schema and it matches" >> {
-      // ue event with same schema
-      // ue event with different schema
-      // non ue event
-      1 must_== 1
+      val shouldKeep = EmptyFilterGood.copy(schema = GoodEvent1.schema)
+      val shouldNotKeep = EmptyFilterGood.copy(schema = GoodEvent2.schema)
+
+      ValidationCache.keepGoodEvent(GoodEvent1, shouldKeep) should beTrue
+      ValidationCache.keepGoodEvent(GoodEvent1, shouldNotKeep) should beFalse
     }
     
     "should correctly return true or false if the filter contains only contexts and it matches" >> {
-      1 must_== 1
+      val shouldKeep = EmptyFilterGood.copy(contexts = Some(GoodEvent1.contexts))
+      val shouldNotKeep = EmptyFilterGood.copy(contexts = Some(GoodEvent2.contexts))
+
+      ValidationCache.keepGoodEvent(GoodEvent1, shouldKeep) should beTrue
+      ValidationCache.keepGoodEvent(GoodEvent1, shouldNotKeep) should beFalse
     }
 
     "should correctly return true or false if several filters are set at the same time" >> {
-      1 must_== 1
+      val shouldKeep = FiltersGood(GoodEvent1.eventType, GoodEvent1.schema, Some(GoodEvent1.contexts), None)
+      val shouldNotKeep1 = FiltersGood(Some("nonsense"), GoodEvent1.schema, Some(GoodEvent1.contexts), None)
+      val shouldNotKeep2 = FiltersGood(GoodEvent1.eventType, Some("nonsense"), Some(GoodEvent1.contexts), None)
+      val shouldNotKeep3 = FiltersGood(GoodEvent1.eventType, GoodEvent1.schema, Some(List("nonsense")), None)
+
+      ValidationCache.keepGoodEvent(GoodEvent1, shouldKeep) should beTrue
+      ValidationCache.keepGoodEvent(GoodEvent1, shouldNotKeep1) should beFalse
+      ValidationCache.keepGoodEvent(GoodEvent1, shouldNotKeep2) should beFalse
+      ValidationCache.keepGoodEvent(GoodEvent1, shouldNotKeep3) should beFalse
     }
   }
 
   "containsAllContext" >> {
     "should return true if the event contains exactly the same contexts" >> {
-      1 must_== 1
+      ValidationCache.containsAllContexts(GoodEvent1, GoodEvent1.contexts) should beTrue
     }
     
     "should return true if the event contains the same contexts and more" >> {
-      1 must_== 1
+      ValidationCache.containsAllContexts(GoodEvent1, GoodEvent1.contexts.tail) should beTrue
     }
     
     "should return true if list of contexts in the filter is empty" >> {
-      1 must_== 1
+      ValidationCache.containsAllContexts(GoodEvent1, Nil) should beTrue
     }
   }
   
   "filterBad" >> {
-    "should return only the bad events for which the filter matches" >> {
-      // put 2 events in cache
-      // filter that returns both of them
-      // filter that returns 1 of them
-      // filter that returns none of them
-      1 must_== 1
+    "should return only the bad events that match the filtered vendor" >> {
+      val cache = cacheOf(Nil, List(BadEvent1, BadEvent2))
+
+      val filter1 = EmptyFilterBad.copy(vendor = Some(CollectorPayload1.api.vendor))
+      val filter2 = EmptyFilterBad.copy(vendor = Some(CollectorPayload2.api.vendor))
+      val filterNonsense = EmptyFilterBad.copy(vendor = Some("nonsense"))
+
+      cache.filterBad(filter1) must contain(exactly(BadEvent1))
+      cache.filterBad(filter2) must contain(exactly(BadEvent2))
+      cache.filterBad(filterNonsense) must be empty
+    }
+
+    "should return only the bad events that match the filtered version" >> {
+      val cache = cacheOf(Nil, List(BadEvent1, BadEvent2))
+
+      val filter1 = EmptyFilterBad.copy(version = Some(CollectorPayload1.api.version))
+      val filter2 = EmptyFilterBad.copy(version = Some(CollectorPayload2.api.version))
+      val filterNonsense = EmptyFilterBad.copy(version = Some("nonsense"))
+
+      cache.filterBad(filter1) must contain(exactly(BadEvent1))
+      cache.filterBad(filter2) must contain(exactly(BadEvent2))
+      cache.filterBad(filterNonsense) must be empty
+    }
+
+    "should return the most recent events if limit is set" >> {
+      val cache = emptyCache()
+      cache.addToBad(List(BadEvent1))
+      cache.addToBad(List(BadEvent2)) // most recent
+
+      val filter = EmptyFilterBad.copy(limit = Some(1))
+      cache.filterBad(filter) must contain(exactly(BadEvent2))
     }
   }
   
   "keepBadEvent" >> {
     "should correctly filter out based on the vendor" >> {
-      1 must_== 1
+      val shouldKeep = EmptyFilterBad.copy(vendor = Some(CollectorPayload1.api.vendor))
+      val shouldNotKeep = EmptyFilterBad.copy(vendor = Some(CollectorPayload2.api.vendor))
+
+      ValidationCache.keepBadEvent(BadEvent1, shouldKeep) should beTrue
+      ValidationCache.keepBadEvent(BadEvent1, shouldNotKeep) should beFalse
     }
     
     "should correctly filter out based on the version" >> {
-      1 must_== 1
+      val shouldKeep = EmptyFilterBad.copy(version = Some(CollectorPayload1.api.version))
+      val shouldNotKeep = EmptyFilterBad.copy(version = Some(CollectorPayload2.api.version))
+
+      ValidationCache.keepBadEvent(BadEvent1, shouldKeep) should beTrue
+      ValidationCache.keepBadEvent(BadEvent1, shouldNotKeep) should beFalse
     }
     
     "should correctly filter out based on the vendor and the version at the same time" >> {
-      1 must_== 1
+      val shouldKeep = FiltersBad(Some(CollectorPayload1.api.vendor), Some(CollectorPayload1.api.version), None)
+      val shouldNotKeep1 = FiltersBad(Some("nonsense"), Some(CollectorPayload1.api.version), None)
+      val shouldNotKeep2 = FiltersBad(Some(CollectorPayload1.api.vendor), Some("nonsense"), None)
+
+      ValidationCache.keepBadEvent(BadEvent1, shouldKeep) should beTrue
+      ValidationCache.keepBadEvent(BadEvent1, shouldNotKeep1) should beFalse
+      ValidationCache.keepBadEvent(BadEvent1, shouldNotKeep2) should beFalse
     }
   }
+}
+
+object ValidationCacheSpec {
+
+  val EmptyFilterGood: FiltersGood = FiltersGood(None, None, None, None)
+  val EmptyFilterBad: FiltersBad = FiltersBad(None, None, None)
+
+  def cacheOf(goodInit: List[GoodEvent], badInit: List[BadEvent]): ValidationCache =
+    new ValidationCache {
+      var good = goodInit
+      var bad = badInit
+    }
+
+  def emptyCache(): ValidationCache =
+    cacheOf(Nil, Nil)
+
+  val GoodEvent1: GoodEvent = 
+    GoodEvent(
+      events.buildRawEvent(),
+      Some("type1"),
+      Some("com.snowplowanalytics.example1"),
+      List("com.snowplowanalytics.context1a", "com.snowplowanalytics.context1b"),
+      new EnrichedEvent()
+    )
+
+  val GoodEvent2: GoodEvent = 
+    GoodEvent(
+      events.buildRawEvent(),
+      Some("type2"),
+      Some("com.snowplowanalytics.example2"),
+      List("com.snowplowanalytics.context2a", "com.snowplowanalytics.context2b"),
+      new EnrichedEvent()
+    )
+
+  val CollectorPayload1: CollectorPayload =
+    CollectorPayload(
+      CollectorPayload.Api("vendor1", "version1"),
+      Nil,
+      None,
+      None,
+      CollectorPayload.Source("name1", "utf-8", None),
+      CollectorPayload.Context(None, None, None, None, Nil, None)
+    )
+
+  val CollectorPayload2: CollectorPayload =
+    CollectorPayload(
+      CollectorPayload.Api("vendor2", "version2"),
+      Nil,
+      None,
+      None,
+      CollectorPayload.Source("name2", "utf-8", None),
+      CollectorPayload.Context(None, None, None, None, Nil, None)
+    )
+
+  val BadEvent1: BadEvent =
+    BadEvent(Some(CollectorPayload1), None, List("event1 is bad"))
+
+  val BadEvent2: BadEvent =
+    BadEvent(Some(CollectorPayload2), None, List("event2 is bad"))
+
 }
