5cd87219f585f994ca1a6edba4580aa2b2a9db30
Paul Boocock <paul@snowplowanalytics.com>
2021-06-03 15:21:24 +0100

    Add CORS headers to /micro endpoints (close #50)

--- src/main/scala/com.snowplowanalytics.snowplow.micro/Routing.scala
+++ src/main/scala/com.snowplowanalytics.snowplow.micro/Routing.scala
@@ -12,13 +12,17 @@
  */
 package com.snowplowanalytics.snowplow.micro
 
-import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.server.{Route, RouteResult}
 import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.model.headers.{`Access-Control-Allow-Methods`, `Access-Control-Allow-Headers`}
+import akka.http.scaladsl.model.{HttpMethods, StatusCodes}
 
 import io.circe.generic.auto._
 
 import com.snowplowanalytics.snowplow.collectors.scalastream.model.{CollectorConfig, CollectorSinks}
 import com.snowplowanalytics.snowplow.collectors.scalastream.{CollectorRoute, CollectorService}
+
+import scala.concurrent.ExecutionContext
 
 import CirceSupport._
 
@@ -39,41 +43,61 @@ private[micro] object Routing {
   def getMicroRoutes(
       collectorConf: CollectorConfig,
       collectorSinks: CollectorSinks
-  ): Route = {
+  )(implicit ec: ExecutionContext): Route = {
+    val c = new CollectorService(collectorConf, collectorSinks)
     val collectorRoutes = new CollectorRoute {
-      override def collectorService =
-        new CollectorService(collectorConf, collectorSinks)
+      override def collectorService = c
     }.collectorRoute
 
-    val microRoutes: Route = pathPrefix("micro") {
-      (get | post) {
-        path("all") {
-          complete(ValidationCache.getSummary())
-        } ~ path("reset") {
-          ValidationCache.reset()
-          complete(ValidationCache.getSummary())
-        }
-      } ~ get {
-        path("good") {
-          complete(ValidationCache.filterGood(FiltersGood(None, None, None, None)))
-        } ~ path("bad") {
-          complete(ValidationCache.filterBad(FiltersBad(None, None, None)))
-        }
-      } ~ post {
-        path("good") {
-          entity(as[FiltersGood]) { filters =>
-            complete(ValidationCache.filterGood(filters))
+    withCors(c) {
+      pathPrefix("micro") {
+        (get | post) {
+          path("all") {
+            complete(ValidationCache.getSummary())
+          } ~ path("reset") {
+            ValidationCache.reset()
+            complete(ValidationCache.getSummary())
           }
-        } ~ path("bad") {
-          entity(as[FiltersBad]) { filters =>
-            complete(ValidationCache.filterBad(filters))
+        } ~ get {
+          path("good") {
+            complete(ValidationCache.filterGood(FiltersGood(None, None, None, None)))
+          } ~ path("bad") {
+            complete(ValidationCache.filterBad(FiltersBad(None, None, None)))
           }
-        } 
-      } ~ {
-        complete("Path for micro has to be one of: /all /good /bad /reset")
+        } ~ post {
+          path("good") {
+            entity(as[FiltersGood]) { filters =>
+              complete(ValidationCache.filterGood(filters))
+            }
+          } ~ path("bad") {
+            entity(as[FiltersBad]) { filters =>
+              complete(ValidationCache.filterBad(filters))
+            }
+          }
+        } ~ options {
+          complete(StatusCodes.OK)
+        } ~ {
+          complete("Path for micro has to be one of: /all /good /bad /reset")
+        }
+      }
+    } ~ collectorRoutes
+  }
+
+  /** Wrap a Route with CORS header handling.
+  *
+  *  Reuses the implementation used by the stream collector
+  */
+  private def withCors(c: CollectorService)(route: Route)(implicit ec: ExecutionContext): Route =
+    extractRequest { request => requestContext =>
+      route(requestContext).map {
+        case RouteResult.Complete(response) =>
+          val r = response.withHeaders(List(
+            `Access-Control-Allow-Methods`(List(HttpMethods.POST, HttpMethods.GET, HttpMethods.OPTIONS)),
+            c.accessControlAllowOriginHeader(request),
+            `Access-Control-Allow-Headers`("Content-Type")
+          ))
+          RouteResult.Complete(r)
+        case other => other
       }
     }
-
-    microRoutes ~ collectorRoutes
-  }
 }
