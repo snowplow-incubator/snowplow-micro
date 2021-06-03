5411c61763dd2e5739452ec402e3d47d07c1637c
Benjamin Benoist <benjamin.benoist@snowplowanalytics.com>
2021-06-03 17:26:53 +0200

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
-[ "$(ls -A /iglu-schemas >/dev/null 2>&1)" ] && python3 -m http.server 8080 --directory /iglu-schemas >/dev/null 2>&1 &
+python3 -m http.server 8080 --directory /iglu-schemas &
 
 /opt/docker/bin/snowplow-micro $@
