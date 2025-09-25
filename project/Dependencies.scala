b0a7413d4ac56cccffd34c0b4b4f5a3c172eac66
spenes <enes@snowplowanalytics.com>
2025-09-25 15:02:53 +0300

    Bump enrich to 6.1.2 (#167)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
   object V {
     // Snowplow
     val snowplowStreamCollector = "3.4.0"
-    val snowplowCommonEnrich    = "5.4.0"
+    val snowplowCommonEnrich    = "6.1.2"
     val http4sCirce             = "0.23.23"
 
     val decline          = "2.4.1"
